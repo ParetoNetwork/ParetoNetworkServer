875b8e271154ddd420ffbc5876fd058e83e21e54
Eric Lamison-White <eric@blockology.org>
2018-03-06 22:31:03 -0800

    more db functions, and concurrency added

--- backend.js
+++ backend.js
@@ -1,71 +1,83 @@
 "use strict";
 
-var express = require('express');
-var path    = require("path");
-var boom = require('express-boom-2');
-var requestp = require('request-promise');
-var controller = require('./backend-controller.js');
-var app = express();
-var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/TnsZa0wRB5XryiozFV0i"));
-var uniqueRandomArray = require('unique-random-array');
-const debug = require('debug')('pareto-ranking')
-const appName = 'Pareto Ranking Backend'
-debug('booting %s', appName)
+const throng = require('throng');
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const PORT = process.env.PORT || 3000;
 
-app.use(boom()); //handles error codes in a consistent way and format
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
 
-//web3Auth.attach(app, "Cryptographically sign this to prove ownership.");
+function start() {
+  var express = require('express');
+  var path    = require("path");
+  var boom = require('express-boom-2');
+  var requestp = require('request-promise');
+  var controller = require('./backend-controller.js');
+  var app = express();
+  var Web3 = require('web3');
+  var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/TnsZa0wRB5XryiozFV0i"));
+  var uniqueRandomArray = require('unique-random-array');
+  const debug = require('debug')('pareto-ranking')
+  const appName = 'Pareto Ranking Backend'
+  debug('booting %s', appName)
 
-/*app.get('/who', function (req, res) {
-  console.log(req.cookies.token);
-  console.log(req.user);
-  if (req.user) {
-    res.json({account: req.user.loggedInAs});
-  }
-  else {
-    res.status(404);
-  }
-  res.end();
-});*/
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static('public'));
 
-app.get('/',function(req,res){
-  //__dirname : It will resolve to your project folder.
-  res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+  app.use(boom()); //handles error codes in a consistent way and format
 
-app.get('/intel',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/intel.html'));
-});
+  //web3Auth.attach(app, "Cryptographically sign this to prove ownership.");
 
-app.get('/summation', function(req, fres){
-    
-    controller.calculateScore(req, fres, web3);
+  /*app.get('/who', function (req, res) {
+    console.log(req.cookies.token);
+    console.log(req.user);
+    if (req.user) {
+      res.json({account: req.user.loggedInAs});
+    }
+    else {
+      res.status(404);
+    }
+    res.end();
+  });*/
 
-});//end entire function
+  app.get('/',function(req,res){
+    //__dirname : It will resolve to your project folder.
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+  });
 
-app.post('/content', function(req, res){
+  app.get('/intel',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/intel.html'));
+  });
 
-  if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-    res.boom.badRequest('POST body missing');
-  }
-  else if(req.body.address === undefined || req.body.title === undefined || req.body.body === undefined ){
-    res.boom.badRequest('POST body missing, needs address, title and body'); 
-  } else {
-    controller.postContent(req.body, res);
-  }
+  app.get('/summation', function(req, fres){
+      
+      controller.calculateScore(req, fres, web3);
 
-}); //end content post
+  });//end entire function
 
-app.get('/content', function(req, res){
+  app.post('/content', function(req, res){
 
-});
+    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+      res.boom.badRequest('POST body missing');
+    }
+    else if(req.body.address === undefined || req.body.title === undefined || req.body.body === undefined ){
+      res.boom.badRequest('POST body missing, needs address, title and body'); 
+    } else {
+      controller.postContent(req.body, res);
+    }
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Pareto Network ranking app listening on port 3000!')
-});
+  }); //end content post
+
+  app.get('/content', function(req, res){
+
+  });
+
+  app.listen(process.env.PORT || 3000, function () {
+    console.log('Pareto Network ranking app listening on port 3000!')
+  });
+}
