da448c4f89f6b31677e707a20a8709f5f5851a41
Eric Lamison-White <eric@blockology.org>
2018-03-06 16:26:55 -0800

    adding POST request

---
+++ backend-controller.js
@@ -0,0 +1,71 @@
+var mongodb = require('mongodb').MongoClient;
+const assert = require('assert');
+var connectionUrl = require('./backend-credentials.json').mongodb_connection;
+
+const dbName = 'pareto';
+
+function postContent (body, res){
+
+	//exposed endpoint to write content to database
+
+	mongodb.connect(connectionUrl, function(err, client) {
+	  assert.equal(null, err);
+	  //console.log("Connected correctly to server");
+
+	  const db = client.db(dbName);
+
+	  // Insert a single document
+	  db.collection('content').insertOne(
+	  	body, function(err, r) {
+	    //assert.equal(null, err);
+	    //assert.equal(1, r.insertedCount);
+	    if(err){
+	    	console.error('unable because: ', err);
+	    	res.status(500).send('unable because: ' + err);
+	    } else {
+	    	res.status(200).json(r);
+	    }
+
+	  });
+	}); //end mongodb
+
+};
+
+module.exports.postContent = postContent;
+
+
+function getAllAvailableContent(){
+
+	//check if user, then return what the user is privy to see
+
+};
+
+function getContentById(){
+
+	//check if user, then check if the user is privy to see it
+
+};
+
+function retrieveScores(){
+
+	//makes the rank by sorting server side, stores all ranks in db, also sends result client side if requested
+
+};
+
+function retrieveRank(){
+
+	//quick way to retrieve the current snapshot of rankings. can limit to a range
+
+};
+
+function retrieveRankAroundAddress(){
+
+	//a range of rankings
+
+};
+
+function calculateAllScores(){
+
+	//way to do all the ranking calculations
+
+};
--- backend.js
+++ backend.js
@@ -1,8 +1,9 @@
 "use strict";
 
 var express = require('express');
-//var request = require('request');
+var boom = require('express-boom-2');
 var requestp = require('request-promise');
+var controller = require('./backend-controller.js');
 var app = express();
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/TnsZa0wRB5XryiozFV0i"));
@@ -11,7 +12,12 @@ const debug = require('debug')('pareto-ranking')
 const appName = 'Pareto Ranking Backend'
 debug('booting %s', appName)
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use(boom()); //handles error codes in a consistent way and format
 
 //web3Auth.attach(app, "Cryptographically sign this to prove ownership.");
 
@@ -62,6 +68,8 @@ app.get('/summation', function(req, fres){
         var outgoing = {};
 
         console.log(address);
+
+        //should re-do 
 
         //get current blocknumber too
         web3.eth.getBlock('latest')
@@ -207,6 +215,23 @@ app.get('/summation', function(req, fres){
     } //end address validation
 
 });//end entire function
+
+app.post('/content', function(req, res){
+
+  if(req.body.constructor === Object && Object.keys(req.body).length === 0){
+    res.boom.badRequest('POST body missing');
+  }
+  else if(req.body.address === undefined || req.body.title === undefined || req.body.body === undefined ){
+    res.boom.badRequest('POST body missing, needs address, title and body'); 
+  } else {
+    controller.postContent(req.body, res);
+  }
+
+}); //end content post
+
+app.get('/content', function(req, res){
+
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Pareto Network ranking app listening on port 3000!')
