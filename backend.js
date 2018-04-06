c902ea067bf74363ab15434854fe1a528e560a71
Eric Lamison-White <eric@blockology.org>
2018-04-06 13:26:43 -0700

    adds limit to entity

--- backend.js
+++ backend.js
@@ -32,8 +32,9 @@ function start() {
 
   var bodyParser = require('body-parser');
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  //the key/value pairs fixes the error PayloadTooLargeError: request entity too large 
+  app.use(bodyParser.json({limit: "50mb"}));
+  app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
   app.use(express.static('public'));
   app.use(cookieParser());
   app.use(compression());
