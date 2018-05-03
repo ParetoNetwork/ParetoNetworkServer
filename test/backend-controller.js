23374ad8b54b097c1b03bd3267e9e97145215360
Andres Jaramillo <jandresjc@gmail.com>
2018-05-03 17:49:40 -0500

    add test for content

--- test/backend-controller.js
+++ test/backend-controller.js
@@ -4,9 +4,31 @@ const assert = require('chai').assert;
 
 describe('backend-controller /', function() {
 
-    it('check mongo connection',  function (done) {
+    it('Check mongo connection',  function (done) {
         assert.isOk(serverApp.controller.mongoose.connection.readyState, 'Mongo is not ready');
-        done()
+        done();
+    });
+
+    it('No update wrong address',  function (done) {
+        serverApp.controller.calculateScore("alsdkfjouasdf", 0, function(err, result){
+            assert.exists(err, 'The controller should validate fake address');
+            done();
+        });
+    });
+
+    it('No get content with wrong address',  function (done) {
+        serverApp.controller.getAllAvailableContent({user: "asdf"}, function(err, result){
+            assert.exists(err, 'The controller should validate fake address');
+            done();
+        });
+    });
+
+    it('Current user get data Test',  function (done) {
+        serverApp.controller.getAllAvailableContent({user: process.env.TEST_ADDRESS}, function(err, result){
+            assert.notExists(err, 'The current data shouldnt get error');
+            assert.exists(result, 'The content must be gotten');
+            done();
+        });
     });
 
 
