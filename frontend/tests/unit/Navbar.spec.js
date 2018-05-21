2616611e7b9b20a26f4965c58460ab35fd2b28ce
Andres Jaramillo <jandresjc@gmail.com>
2018-05-21 16:24:59 -0500

    first test vue

--- frontend/tests/unit/Navbar.spec.js
+++ frontend/tests/unit/Navbar.spec.js
@@ -1,11 +1,28 @@
+import Vuex from 'vuex'
 import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import {mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Navbar from '@/components/Navbar.vue'
+import App from '@/App.vue'
 
-describe('Navbar.vue', () => {
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+describe('App.vue', () => {
   it('renders Navbar click', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(Navbar);
-    expect(wrapper.text()).to.include(msg)
+
+    const wrapper = shallowMount(Navbar, {
+        mocks: {
+            $store: {
+                state: {
+                    isLogged: false
+                }
+            }
+        },
+        localVue
+    });
+    const button = wrapper.find('#navbarDropdown');
+    console.log("HOLA")
+    button.trigger('click');
+    expect(wrapper.html()).contain('No user AUTHENTICATED');
   })
 });
