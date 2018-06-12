a988be085bf127605afc9f66f095c4965a6a0d32
Andres Jaramillo <jandresjc@gmail.com>
2018-06-12 13:20:30 -0500

    upload images profile

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
 var compression = require('compression');
 var express = require('express');
+const fs = require("fs");
 const expressStaticGzip = require('express-static-gzip');
 var path = require('path');
 var boom = require('express-boom-2');
 var requestp = require('request-promise');
 var jwt = require('jsonwebtoken');
 var cookieParser = require('cookie-parser');
-
+const multer = require("multer");
 var controller = require('./backend-controller.js');
 
 var app = express();
@@ -20,6 +21,19 @@ const appName = 'Pareto Ranking Backend';
 debug('booting %s', appName);
 
 var bodyParser = require('body-parser');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const dir = path.join(__dirname, "/images");
+
+        fs.mkdir(dir, err => cb(err, dir))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+
+var upload = multer({ storage: storage });
 
 //the key/value pairs fixes the error PayloadTooLargeError: request entity too large
 app.use(bodyParser.json({limit: '50mb'}));
@@ -64,6 +78,17 @@ const MongoStore = require('connect-mongo')(session);
   res.end();
 });*/
 
+app.post('/upload-profile', upload.single('file'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    res.status(200).json({filename: req.file.filename});
+});
+
+app.get('/profile-image', function (req, res) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+    res.sendFile( path.join(__dirname, "/images/"+req.query.image));
+});
 
 app.get('/', function (req, res) {
     //__dirname : It will resolve to your project folder.
@@ -419,6 +444,7 @@ app.post('/v1/updateranks', function (req, res) {
         }
     } //end else
 });
+
 
 app.use('/public/static/', expressStaticGzip('/public/static/', {
     customCompressions: [{
