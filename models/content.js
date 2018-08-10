67ab2f666cac99b121a483b66cb8f3ee6fb9a26d
Hans Ospina <hansospina@gmail.com>
2018-08-10 08:36:37 -0500

    add createdBy

--- models/content.js
+++ models/content.js
@@ -12,8 +12,14 @@ var paretoContentSchema = new Schema({
 	dateCreated : { type: Date, default: Date.now },
 	block 	: Number
 
-}, { collection : 'content' });
+}, { collection : 'content' , toObject : {virtuals:true},toJSON: { virtuals: true } });
 
+paretoContentSchema.virtual('createdBy', {
+    ref: 'profile', // The model to use
+    localField: 'address', // Find post where `localField`
+    foreignField: 'address', // is equal to `foreignField`
+    justOne: true
+});
 const ParetoContent = mongoose.model('content', paretoContentSchema);
 
 module.exports = ParetoContent;
