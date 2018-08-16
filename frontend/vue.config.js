adee0f45404abfbf64f5118871417d3ddcd644f7
CarlosADLRT <cadelosreyest@gmail.com>
2018-08-16 16:11:48 -0500

    Fixes #80 added sources to mp4, ogg and webm

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,6 +7,17 @@ module.exports = {
                         test: require.resolve('snapsvg'),
                         use: 'imports-loader?this=>window,fix=>module.exports=0',
                     },
+                    {
+                        test: /\.(ogg|ogv)$/,
+                        use: [
+                            {
+                                loader: 'file-loader',
+                                options: {
+                                    name: '[name].[ext]'
+                                }
+                            }
+                        ]
+                    }
 
             ]
         }
