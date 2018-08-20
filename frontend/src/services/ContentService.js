02b4b301cb4856cc29a1f4f24bfe23634c0b210a
Sheraz Arshad <sheraz1392@gmail.com>
2018-08-20 07:31:00 +0300

    add send reward from front-end functionality

--- frontend/src/services/ContentService.js
+++ frontend/src/services/ContentService.js
@@ -57,16 +57,16 @@ export default class ContentService {
           Pareto_Token_Schema.networks["3"].address
         );
 
-        const _ttl = Math.round(new Date().getTime() / 1000) + 174; // add five seconds to to allow the rewarder to reward pareto tokens
+        const _ttl = Math.round(new Date().getTime() / 1000) + 1111174; // add five seconds to to allow the rewarder to reward pareto tokens
         const depositAmount = 2 * 10 ** 18;
         const desiredReward = "4";
 
         let gasApprove = await ParetoTokenInstance.methods
-          .approve(Intel.options.address, depositAmount*5)
-          .estimateGas({ from: provider_address })
-          
+          .approve(Intel.options.address, depositAmount)
+          .estimateGas({ from: provider_address });
+
         await ParetoTokenInstance.methods
-          .approve(Intel.options.address, depositAmount*5)
+          .approve(Intel.options.address, depositAmount)
           .send({ from: provider_address, gas: gasApprove })
           .on("error", err => {
             onError(err);
@@ -76,18 +76,17 @@ export default class ContentService {
           .create(
             provider_address,
             depositAmount,
-            web3.utils.toWei(desiredReward, 'ether'),
+            web3.utils.toWei(desiredReward, "ether"),
             content.ID,
             _ttl
           )
-          .estimateGas({ from: provider_address })
-          
+          .estimateGas({ from: provider_address });
 
         await Intel.methods
           .create(
             provider_address,
             depositAmount,
-            web3.utils.toWei(desiredReward, 'ether'),
+            web3.utils.toWei(desiredReward, "ether"),
             content.ID,
             _ttl
           )
@@ -99,12 +98,66 @@ export default class ContentService {
             onError(err);
           });
 
-
         onSuccess("successfull");
-        // Call sendReward function on Intel contract to send reward to an Intel
-        // const gasSendReward = await Intel.methods.sendReward(content.ID, depositAmount*3).estimateGas({ from: provider_address });
-        // await Intel.methods.sendReward(content.ID, depositAmount*3).send({ from: provider_address, gas: gasSendReward });
+        
+      });
+    }
+  }
 
+  static rewardIntel(content, onSuccess, onError) {
+      console.log(content,"content");
+    let provider;
+    if (typeof window.web3 !== "undefined") {
+      // Use Mist/MetaMask's provider
+      provider = new Web3(window.web3.currentProvider);
+    } else {
+      console.log("No web3? You should consider trying MetaMask!");
+      onError(
+        "Please install MetaMask (or other web3 browser) in order to access the Pareto Network"
+      );
+
+      // searchLookup();
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+      provider = new Web3(
+        new Web3.providers.HttpProvider(
+          "https://ropsten.infura.io/QWMgExFuGzhpu2jUr6Pq"
+        )
+      );
+    }
+    const web3 = new Web3(provider);
+    if (typeof provider !== "undefined") {
+      web3.eth.getAccounts(async (err, accounts) => {
+        if (err) {
+          onError("Err getting accounts");
+          return;
+        }
+        const rewarder_address = accounts[0];
+        const Intel = new web3.eth.Contract(
+          Intel_Contract_Schema.abi,
+          Intel_Contract_Schema.networks["3"].address
+        );
+
+        const ParetoTokenInstance = new web3.eth.Contract(
+          Pareto_Token_Schema.abi,
+          Pareto_Token_Schema.networks["3"].address
+        );
+
+        const depositAmount = 10 * 10 ** 18;
+
+        let gasApprove = await ParetoTokenInstance.methods
+          .approve(Intel.options.address, depositAmount)
+          .estimateGas({ from: rewarder_address });
+
+        await ParetoTokenInstance.methods
+          .approve(Intel.options.address, depositAmount)
+          .send({ from: rewarder_address, gas: gasApprove })
+          .on("error", err => {
+            onError(err);
+          });
+
+        const gasSendReward = await Intel.methods.sendReward(content.ID, depositAmount).estimateGas({ from: rewarder_address });
+        await Intel.methods.sendReward(content.ID, depositAmount).send({ from: rewarder_address, gas: gasSendReward });
+        onSuccess("success")
       });
     }
   }
