228631ad5b87cd7a49cd0c163c329dfbb5597df9
Hans Ospina <hansospina@gmail.com>
2018-09-10 15:00:44 -0500

    Missing custom configuration

--- frontend/src/components/Modals/VModalLedgerNano.vue
+++ frontend/src/components/Modals/VModalLedgerNano.vue
@@ -25,7 +25,7 @@
                     </div>
                     <br/>
 
-                    <b-row>-------------------------------------------------
+                    <b-row>
                         <div class="ml-3 mb-2">
                             <p v-if="!supported" class="text-danger"> Your browser does not support this feature </p>
                             <p v-else class="text-primary"> Choose a HD path: </p>
@@ -35,7 +35,7 @@
                         <b-form-radio-group v-model="selectedPath">
                             <b-row class="m-2 mt-4">
                                 <div class="col-md-3 mb-2 p-0">
-                                    <b-form-radio :disabled="!supported" v-on:change="onPathSelected('44\'/60\'/0\'/0/0', 'standard', standard)" value="44'/60'/0'/0/0">
+                                    <b-form-radio :disabled="!supported" v-on:change="onPathSelected('44\'/60\'/0\'/0/0')" value="44'/60'/0'/0/0">
                                         <div class="modal-input-inline">
                                             <p> STANDARD </p> <p>"44'/60'/0'/0/0"</p>
                                         </div>
@@ -52,7 +52,7 @@
 
                             <b-row class="m-2 mt-4">
                                 <div class="col-md-3 mb-2 p-0">
-                                    <b-form-radio :disabled="!supported" v-on:change="onPathSelected('44\'/60\'/0\'/0', 'legacy', legacy)" value="44'/60'/0'/0">
+                                    <b-form-radio :disabled="!supported" v-on:change="onPathSelected('44\'/60\'/0\'/0')" value="44'/60'/0'/0">
                                         <div class="modal-input-inline">
                                             <p> LEGACY </p> <p>"44'/60'/0'/0"</p>
                                         </div>
@@ -69,18 +69,23 @@
 
                             <b-row class="m-2 mt-4">
                                 <div class="col-12 col-md-4 mb-2 p-0">
-                                    <b-form-radio>
+                                    <b-form-radio :disabled="!supported" v-bind:value="customPath">
                                         <b-form-input
                                                 :disabled="!supported"
                                                 v-model="customPath"
-                                                v-on:keyup="onPathSelected(customPath)"
+                                                v-on:keyup="keymonitor"
                                                 id="form-custom-input"
                                                 class="p-1"
                                                 type="text"
-                                                placeholder="CUSTOM"></b-form-input></b-form-radio>
+                                                placeholder="CUSTOM"></b-form-input>
+                                    </b-form-radio>
                                 </div>
                                 <div class="col-12 ml-md-3 col-md-7 p-0">
-                                    <b-form-select :disabled="!supported" class="mb-3"/>
+                                    <b-form-select id="custom"
+                                                   :disabled="!supported" class="mb-3"/>
+                                </div>
+                                <div class="col-12">
+                                    <p v-if="selectedPath === customPath && customPathError" class="text-danger"> The written path doesn't have any addresses: </p>
                                 </div>
                             </b-row>
                         </b-form-radio-group>
@@ -106,48 +111,132 @@
         data(){
             return {
                 supported : true,
-                selectedPath: "44'/60'/0'/0/0",
-                selectedAddress :'',
-                customPath : '',
-                selectedInput : [
-                  '#standard',
-                  '#legacy'
-                ],
-                standard :
-                {
-                    "1": "0X001",
-                    "2": "0X002",
-                    "3": "0X003",
-                    "4": "0X004",
-                    "5": "0X005",
-                    "6": "0X006",
-                    "7": "0X007",
-                    "8": "0X008",
-                    "9": "0X009",
-                    "0": "0X000"
-                }
-                ,
-                legacy :
-                {
-                    "1": "0X0010",
-                    "2": "0X0020",
-                    "3": "0X0030",
-                    "4": "0X0040",
-                    "5": "0X0050",
-                    "6": "0X0060",
-                    "7": "0X0070",
-                    "8": "0X0080",
-                    "9": "0X0090",
-                    "0": "0X0000"
-                }
+                selectedPath: "",
+                selectedAddress: '',
+                customPath: '',
+                customPathError : true,
+                timer : {},
+                paths: [
+                    {
+                        name: 'standard',
+                        id: "44'/60'/0'/0/0",
+                        address: {
+                            "1": "0X001",
+                            "2": "0X002",
+                            "3": "0X003",
+                            "4": "0X004",
+                            "5": "0X005",
+                            "6": "0X006",
+                            "7": "0X007",
+                            "8": "0X008",
+                            "9": "0X009",
+                            "0": "0X000"
+                        }
+                    },
+                    {
+                        name: 'legacy',
+                        id: "44'/60'/0'/0",
+                        address: {
+                            "1": "0X0010",
+                            "2": "0X0020",
+                            "3": "0X0030",
+                            "4": "0X0040",
+                            "5": "0X0050",
+                            "6": "0X0060",
+                            "7": "0X0070",
+                            "8": "0X0080",
+                            "9": "0X0090",
+                            "0": "0X0000"
+                        }
+                    },
+                    {
+                        name: 'custom',
+                        id: "44'/60'/10'/0",
+                        address: {
+                            "1": "1X0011",
+                            "2": "1X0021",
+                            "3": "1X0031",
+                            "4": "1X0041",
+                            "5": "1X0051",
+                            "6": "1X0061",
+                            "7": "1X0071",
+                            "8": "1X0081",
+                            "9": "1X0091",
+                            "0": "1X0001"
+                        }
+                    }
+                ]
             };
+        },
+        watch: {
+          'customPath' : function (cpath) {
+              this.customPathError = false;
+              if (this.timer) {
+                  clearTimeout(this.timer);
+                  this.timer = null;
+              }
+              this.timer = setTimeout(() => {
+                  this.customPathError = true;
+                  let page = 0, limit = 10;
+                  authService.getWalletAccounts(cpath, page, limit, data=>{
+                      console.log(data);
+                  }, error => {
+                      const select = $('#custom');
+                      select.find('option')
+                          .remove()
+                          .end();
+
+                      this.paths.forEach(path => {
+                          if (path.id === cpath) {
+                              this.customPathError = false;
+                              let list = Object.values(path.address);
+
+                              let option = '';
+                              list.forEach(item => {
+                                  option += '<option value="' + item + '">' + item + '</option>';
+                              });
+                              select.append(option);
+                          }
+                      });
+                  });
+              }, 800);
+          }
         },
         mounted(){
             this.$refs.ledgerNano.show();
-            this.onPathSelected('44\'/60\'/0\'/0/0', 'standard', this.standard)
+            this.fillPathAddress();
+            //this.onPathSelected('44\'/60\'/0\'/0/0', 'standard', this.standard)
             // this.supportedNav();
         },
         methods: {
+            keymonitor: function(){
+              console.log('something')
+            },
+            fillPathAddress: function() {
+                let page = 0, limit = 10;
+
+                this.paths.forEach(path => {
+
+                    if(path.name === 'custom') return;
+
+                    authService.getWalletAccounts(path.id, page, limit, data=>{
+                        console.log(data);
+                    }, error => {
+
+                        this.selectedPath = path.id;
+                        this.onPathSelected(this.selectedPath);
+
+                        const select = $('#' + path.name);
+                        const list = Object.values(path.address);
+
+                        let option = '';
+                        list.forEach( item => {
+                            option += '<option value="'+ item + '">' + item + '</option>';
+                        });
+                        select.append(option);
+                    });
+                });
+            },
             onClosedModal: function () {
                 this.$store.state.showModalLedgerNano = false;
             },
@@ -156,31 +245,16 @@
                     $('#navbarSupportedContent').collapse('toggle');
                 }
             },
-            onPathSelected : function(path, selectInput, object){
-                this.selectedAddress = '';
+            onPathSelected : function(path){
 
-                console.log(path);
+                this.selectedPath = path;
 
-                this.selectedInput.forEach(id => {
-                    $(id)
-                        .find('option')
-                        .remove()
-                        .end()
-                });
-
-                let page = 0, limit = 10;
-
-                authService.getWalletAccounts(path, page, limit, data=>{
-                    console.log(data);
-                }, error => {
-                    const select = $('#' + selectInput);
-                    const list = Object.values(object);
-                    this.selectedAddress = list[0];
-                    let option = '';
-                    list.forEach( item => {
-                        option += '<option value="'+ item + '">' + item + '</option>';
-                    });
-                    select.append(option);
+                this.paths.forEach( path => {
+                   if (path.id === this.selectedPath){
+                       this.selectedAddress = path.address[0];
+                   }else{
+                       return;
+                   }
                 });
             },
             hardware: function () {
