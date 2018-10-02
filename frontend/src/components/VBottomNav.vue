d35bd1093460858bc88b32009a041ce287359b34
CarlosADLRT <cadelosreyest@gmail.com>
2018-10-02 18:17:14 -0500

    Fixes #211 Changed from ETHER to Gwei

--- frontend/src/components/VBottomNav.vue
+++ frontend/src/components/VBottomNav.vue
@@ -3,13 +3,12 @@
         <div class="responsize-positioning d-flex flex-row">
             <a class="mr-3" href="https://ethereum.org"><img style="height: 23px;" src="../assets/images/powered-by-ethereum-small.png" alt=""></a>
             <div class="mr-3"> PARETO PRICE: {{paretoUSD}} </div>
-            <div class="mr-3"> RECOMMENDED ETHEREUM GAS PRICE: {{recommendedGasPrice}} ETHER </div>
+            <div class="mr-3"> RECOMMENDED ETHEREUM GAS PRICE: {{recommendedGasPrice}} gwei </div>
             <!-- <i class="fa fa-circle mr-1" style="color: #32CD32; padding: 2px;"></i>
             <div class="mr-3"> STATUS: CONNECTED </div>
             <div class="mr-3"> RECOMMENDED GAS PRICE: 23 GWEI</div> -->
         </div>
         <div class="d-flex flex-row">
-
             <!--
             <div class="mr-3"> CONTACT </div>
             <div class="mr-3"> TERMS OF USE </div>
@@ -62,12 +61,12 @@
                         type: 'error',
                         duration: 10000,
                         title: 'Content',
-                        text: err
+                        text: error
                     });
                 });
             },
             numberToCurrency: function (number) {
-                return number.toLocaleString('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 5});
+                return number.toLocaleString('en-IN', {style: 'currency', currency: 'USD', minimumFractionDigits: 5});
             }
         }
     }
