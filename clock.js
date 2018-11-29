bef36778f2370d858731a8af05a8d2fe5fc45c2a
Hans Ospina <hansospina@gmail.com>
2018-11-29 10:25:13 -0500

    Fixes #379 add historical snapshot everyday, add + - = in redis for attributes rank, score and tokens

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 
 var clock = module.exports = {};
 clock.time = 0;
+clock.snapshot = true;
 clock.start= function(wqueue){
     const cron = require("node-cron");
     const kue  = require ('kue');
@@ -20,9 +21,9 @@ clock.start= function(wqueue){
     setTimeout(function run() {
         try{
             const time = (new Date().getTime());
-
+            const minutes = clock.snapshot? 1440: ((clock.time === 0)? 5: 1);
             const job = queue
-                .create('clock-job',{minutes: (clock.time === 0)? 5: 1} )
+                .create('clock-job',{minutes} )
                 .removeOnComplete(true)
                 .save((error) => {
                     if (error) {
@@ -30,6 +31,9 @@ clock.start= function(wqueue){
                         return;
                     }
                     job.on('complete', result => {
+                        if(result==='snapshot'){
+                            clock.snapshot = false
+                        }
                         console.log('Sucessfully updated aprox' );
                         clock.time=(clock.time === 4)? 0: clock.time+1;
                         setTimeout(run, Math.max(100, 60000 - (new Date().getTime()) + time ));
@@ -47,6 +51,10 @@ clock.start= function(wqueue){
         }
 
     }, 60000);
+
+    cron.schedule('0 0 0 * * *', () => {
+        clock.snapshot = true
+    });
 }
 
 
