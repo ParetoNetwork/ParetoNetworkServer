46f817e639f87b6b754ab72e84413a49f11dabd9
Hans Ospina <hansospina@gmail.com>
2018-12-06 10:53:42 -0500

    Fixes #355 Fixed Repeated API calls in various views, created cached values on some vue services, fixed little bug on leaderboard and intel feed scroll and some other changes

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -3,7 +3,7 @@ import http from './HttpService';
 export default class dashboardService {
 
     static getAddress(onSuccess, onError) {
-        console.log(this.getAddress.address);
+
         if (this.getAddress.address) {
             return onSuccess(this.getAddress.address);
         } else {
@@ -23,20 +23,6 @@ export default class dashboardService {
             });
         }
     }
-
-    // static getIntel(onSuccess, onError) {
-    //     http.get('/intel', {
-    //         withCredentials: true
-    //     }).then(res => {
-    //         if(res.data.success){
-    //             return onSuccess(res.data.data);
-    //         }else{
-    //             return onError(res.data.message);
-    //         }
-    //     }).catch(error => {
-    //         return onError(error);
-    //     });
-    // }
 
     static getIntel(intel, onSuccess, onError) {
         return http.get('/v1/content/' + intel).then(res => {
@@ -70,7 +56,7 @@ export default class dashboardService {
     static getAllContent(params, onSuccess, onError) {
         const {limit, page} = params || {limit: 10, page: 0};
         let query_params = window.location.hash.split('?');
-        let total_query = ""
+        let total_query = "";
         if (query_params.length > 0) {
             total_query = "&" + query_params[1];
         }
