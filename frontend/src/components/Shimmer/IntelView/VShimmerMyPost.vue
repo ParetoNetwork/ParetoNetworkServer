8449865f97bb3ff11482361adff3b7b8dd44e1f6
Hans Ospina <hansospina@gmail.com>
2019-03-14 18:13:27 -0500

    Various changes to layout and general css

--- frontend/src/components/Shimmer/IntelView/VShimmerMyPost.vue
+++ frontend/src/components/Shimmer/IntelView/VShimmerMyPost.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="intel-container p-4">
-        <div class="dark-shine border thick-medium-lines"></div>
+        <div class="dark-shine thick-medium-lines"></div>
         <ul id="mypost" class="list-unstyled list-group scrollable">
             <li v-for="n in 10" class="border-0 mt-4">
                 <div class="pb-1">
                     <div class="d-flex justify-content-between">
                         <div class="w-100">
-                            <div class="dark-shine border thick-lines"></div>
-                            <div class="dark-shine border medium-lines"></div>
+                            <div class="dark-shine thick-lines"></div>
+                            <div class="dark-shine medium-lines"></div>
                         </div>
-                        <div class="dark-shine border medium-lines ml-3"></div>
+                        <div class="dark-shine medium-lines ml-3"></div>
                     </div>
                 </div>
             </li>
