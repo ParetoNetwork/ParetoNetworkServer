5b7513ceb743251ebdd577fcfd364f2833024f22
Hans Ospina <hansospina@gmail.com>
2019-05-06 17:54:30 -0500

    add redis

--- frontend/src/components/VHandleStackGroupBars.vue
+++ frontend/src/components/VHandleStackGroupBars.vue
@@ -15,36 +15,7 @@
     },
     mounted() {
       chartService.getStackedGroupedInformation(function(res){
-        const transactionsRewards = res;
-        let transactionByMonth = {};
-
-        let intelContractDeposit = 0;
-        transactionsRewards.forEach(transaction => {
-          const date = new Date(transaction.dateCreated);
-          const dateMonthString =  `${date.getMonth()}/${date.getFullYear()}`;
-
-          if(!transactionByMonth[dateMonthString]){
-            transactionByMonth[dateMonthString] = {
-              reward: 0,
-              create: 0,
-              deposited: 0
-            };
-            transactionByMonth[dateMonthString][transaction.event] = transaction.amount;
-          }else{
-            transactionByMonth[dateMonthString][transaction.event] += transaction.amount;
-          }
-
-          if(transaction.event === 'create' || transaction.event === 'reward'){
-            intelContractDeposit -= transaction.amount;
-          }else if(transaction.event === 'deposited'){
-            intelContractDeposit += transaction.amount
-          }
-          if(intelContractDeposit < 0) intelContractDeposit = 0;
-
-          transactionByMonth[dateMonthString].intelContractDeposit = intelContractDeposit;
-        });
-
-        console.log(transactionByMonth);
+        console.log(res);
       });
     }
   }
