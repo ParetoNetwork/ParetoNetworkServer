ecf5c765ff918f35e73a7cbf69ec2b8353fefad6
Eric Lamison-White <eric@blockology.org>
2019-05-09 08:46:59 -0700

    compact exemption for intel retrieval

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -31,7 +31,7 @@ export default class dashboardService {
   }
 
   static getIntel(intel, onSuccess, onError) {
-    return http.get('/v1/content/?compact=true' + intel).then(res => {
+    return http.get('/v1/content/' + intel).then(res => {
       if (res.data.success) {
         return onSuccess(res.data.data);
       } else {
